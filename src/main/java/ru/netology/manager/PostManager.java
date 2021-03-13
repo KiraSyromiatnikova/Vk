40fe630bcc2790139adbd86dfb13b7b6a8cbed7b
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-03-13 21:36:07 +0300

    manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,47 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    Post post = new Post();
+
+    public Post[] getPostsToArray() {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(int ownerID) {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(String domain) {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(String query) {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(boolean ownersOnly) {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(int count) {
+        //TODO: add logic
+        return null;
+    }
+
+    public Post[] search(int offset) {
+        //TODO: add logic
+        return null;
+    }
+
+
+    public void delete(int postID, int ownerID) {
+        //to find the post in Array and delete it
+    }
