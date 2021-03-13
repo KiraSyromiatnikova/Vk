a940b4a26ebf00d78173754c5e463cf0ec861dbc
Kira Syromiatnikova <pereroyal@yandex.ru>
2021-03-13 14:49:41 +0300

    V1

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private int postID;
+    private String postAuthor;
+    private String postTime;
+    private String postText;
+    private String postAttachment;
+    private int viewsQuantity;
+    private int likesQuantity;
+    private int repostsQuantity;
+    private CommentsInfo commentsInfo;
+
+}
